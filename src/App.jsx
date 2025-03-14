78bd4bce69169860dbf7eee1a3d8386fce45ae80
Maria Seiser <seiser@student.tugraz.at>
2025-03-14 17:25:15 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import {useRef} from 'react'
-import {useFrame} from '@react-three/fiber'
-
-
-const RotatingBox = () => {
-
-  const meshRef = useRef();
-
-  useFrame(() => {
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.01;
-  });
-
-  return (
-    <mesh ref={meshRef}>
-      <cylinderGeometry args={[1,1,1]}/>
-      <meshLambertMaterial color={0x02394A} emissive={0x02394A}/>
-
-    </mesh>
-  );
-}
+import React from "react";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
-    <>
-    <Canvas camera={{position: [1,1,1]}}>
-      <color attach="background" args={['lightblue']}/>
-      <OrbitControls/>
-      <directionalLight position={[0, -1, -1]} intensity={20}/>
-      <RotatingBox/>
-    </Canvas>
-    </>
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+    </main>
   );
 }
 
