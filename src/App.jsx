6c2c2bca0568922b960a646616ea501bfa9fb1c5
Maria Seiser <seiser@student.tugraz.at>
2025-03-14 13:37:06 +0100

    update main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import {useRef} from 'react'
+import {useFrame} from '@react-three/fiber'
 
-function App() {
+
+const RotatingBox = () => {
+
+  const meshRef = useRef();
+
+  useFrame(() => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1,1,1]}/>
+      <meshLambertMaterial color={0x02394A} emissive={0x02394A}/>
+
+    </mesh>
+  );
+}
+
+const App = () => {
   return (
     <>
-    <Canvas camera={{position: [3,3,3]}}>
+    <Canvas camera={{position: [1,1,1]}}>
       <color attach="background" args={['lightblue']}/>
       <OrbitControls/>
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshNormalMaterial/>
-      </mesh>
-
+      <directionalLight position={[0, -1, -1]} intensity={20}/>
+      <RotatingBox/>
     </Canvas>
     </>
   );
